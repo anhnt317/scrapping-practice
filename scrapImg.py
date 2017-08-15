6ec3fc8ba0a120730f7a2637bc203b9eb4d84cf8
Nguyen Tuan Anh <anhnt317@gmail.com>
2017-08-15 16:17:39 +0700

    simple extract image from html using regex for select img

---
+++ scrapImg.py
@@ -0,0 +1,22 @@
+# this demonstrate scrap product image from http://www.pythonscraping.com/pages/page3.html avoid logo image
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bsObj = BeautifulSoup(html.read(), 'html.parser')
+images = bsObj.findAll('img', {'src':re.compile('\.\./img/gifts/img.*\.jpg')})
+for image in images:
+    print(image['src'])
+
+
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+# import re
+#
+# html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+# bsObj = BeautifulSoup(html)
+# images = bsObj.findAll('img', {'src':re.compile('\.\.\/img\/gifts/img.*\.jpg')})
+# for image in images:
+#     print(image['src'])
--- scrapTest.py
+++ scrapTest.py
@@ -1,3 +1,45 @@
-import sys
+# import sys
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
 
-print('Python %s \n\non: %s. \n\nWorking path: %s' % (sys.version, sys.platform, sys.path))
+# print('Python %s \n\non: %s. \n\nWorking path: %s' % (sys.version, sys.platform, sys.path))
+
+
+try:
+    html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+except HTTPError as e:
+    print(e)
+else:
+    html = urlopen("http://www.pythonscraping.com/pages/page2.html")
+    bsObj = BeautifulSoup(html.read(), 'html.parser')
+    print(bsObj.head)
+    print(bsObj.h1)
+    print(bsObj.title)
+
+
+def get_title(url):
+    """
+
+    :rtype: return title of url, incase error or not found return None
+    """
+    try:
+        html1 = urlopen(url)
+    except HTTPError as html_err:
+        print('Error occur in HTT' %html_err)
+        return None
+    try:
+        bs = BeautifulSoup(html1.read(), 'html.parser')
+        title1 = bs.html
+    except AttributeError as ea:
+        print('Error in attributes %s' % ea)
+        return None
+    return title1
+
+
+title = get_title('http://www.pythonscraping.com/pages/page2.html')
+
+if title is None:
+    print('Title could not be found')
+else:
+    print(title)
